b7ebc044552575fc244c96841c3fbc25dfc6e135
Tim Kuhlman <tim@backgroundprocess.com>
2020-07-31 11:37:22 -0600

    Split into two library crates for PWA and the pwdb implementations

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
-# I need to make this into a set of github actions or build configs for the app or something
+# TODO I need to make this into a set of github actions or build configs for the app or something
+
+# pwdb is easy running `cargo build` works, all the below instructions are for running in the pwa dir
+
+# in PWA run to create all the associated JSON files
 wasm-pack build --target web --out-name wasm --out-dir ./static
 
-# `python3 -m http.server` in the root is the easiest way (if already installed) to run it for debugging
+# Running `python3 -m http.server` in the root pwa is the easiest way (if already installed) to run it for debugging
 # an alternative is `cargo install miniserver` folllowed by `miniserve ./static --index index.html`
 # for either then browse to http://127.0.0.1:8000
 
